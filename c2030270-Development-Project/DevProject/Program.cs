3ddb19735dbec69b8dedde7e2f068ec78cc6d389
donkeytoadd <jordan.aaron.hanson@gmail.com>
2025-09-18 11:44:31 +0100

    setup c# development project + unit tests project. add swagger support for API that will be developed later on. also setup framework for angular frontend

---
+++ c2030270-Development-Project/DevProject/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "Dev Project API", 
+        Version = "v1",
+        Description = "This will be an API"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dev Project API v1");
+    c.RoutePrefix = "swagger";
+});
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
