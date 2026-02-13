b40f97b00ffff9a46960285919eb2d66900f8861
donkeytoadd <jordan.aaron.hanson@gmail.com>
2026-02-13 13:44:21 +0000

    Add FileUpload Controller to handle file uploads. Returns OK result if success, but if file fails size or extension check, user gets BadRequest result

---
+++ DevProject/Controllers/FileUploadController.cs
@@ -0,0 +1,61 @@
+﻿namespace DevProject.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly ILogger<FileUploadController> logger;
+        
+        private readonly string[] allowedExtensions = { ".xlsx", ".xls"};
+        private readonly long maxFileSize = 10 * 1024 * 1024;
+        
+        public FileUploadController(
+            ILogger<FileUploadController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            try
+            {
+                if (file.Length == 0)
+                {
+                    return BadRequest("No File Selected");
+                }
+                
+                var fileExtension = Path.GetExtension(file.FileName).ToLower();
+                
+                if (!allowedExtensions.Contains(fileExtension))
+                {
+                    return BadRequest($"Only {string.Join(",", allowedExtensions)} are allowed");
+                }
+
+                if (file.Length > maxFileSize)
+                {
+                    return BadRequest($"File size cannot be larger than {maxFileSize} MB");
+                }
+                
+                var uploadsDirectory =  Path.Combine(Directory.GetCurrentDirectory(),"wwwroot", "uploads");
+                Directory.CreateDirectory(uploadsDirectory);
+
+                var fileName = $"{Guid.NewGuid()}{fileExtension}";
+                var filePath = Path.Combine(uploadsDirectory, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                
+                return Ok(fileName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- DevProject/Program.cs
+++ DevProject/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHealthChecks();
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -9,13 +11,27 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo 
     { 
-        Title = "Dev Project API", 
-        Version = "v1",
-        Description = "This will be an API"
+        Title = "Dev Project API"
     });
 });
 
+var myAllowSpecificOrigins = "myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policyBuilder =>
+        {
+            policyBuilder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
